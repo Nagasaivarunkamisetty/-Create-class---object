dbbd5699e9634f25119914df81ea4585cf98a15d
Nagasaivarunkamisetty <70318389+Nagasaivarunkamisetty@users.noreply.github.com>
2020-08-27 06:54:33 -0700

    Add files via upload

---
+++ Whiler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Transactions;
+
+namespace whiler
+{
+    class Whiler
+{
+    static void Main(string[] args)
+    {
+     int i=1,c,j;
+            string var;
+ Console.WriteLine("enter a number"); 
+         var = Console.ReadLine();
+            c = Convert.ToInt32(var);        
+            
+            while (i <= 10)
+        {
+                j = c * i;
+            Console.WriteLine(j);
+            i++;
+        }
+    }
+}
+}
