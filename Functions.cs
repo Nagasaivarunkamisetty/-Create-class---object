41e58bee7ca5c46e8608c564ed6a47afc891ac83
Nagasaivarunkamisetty <70318389+Nagasaivarunkamisetty@users.noreply.github.com>
2020-08-27 06:33:09 -0700

    Add files via upload

---
+++ Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    public class Functions
+    {
+      void greatest(int a, int b, int c)
+        {
+            if (a > b && a > c)
+                Console.WriteLine("a is greatest");
+            else if (b > a && b > c)
+                Console.WriteLine("b is greatest");
+            else
+                Console.WriteLine("c is greatest");
+
+
+        }
+     public static void Main(string[] args)
+        {
+            Functions varun = new Functions();
+            varun.greatest(4, 5, 6);
+
+        
+        }
+    }
+}
