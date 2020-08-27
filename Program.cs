6e4ba7a1070ba8b109719d9973742ced7bd87a4d
Nagasaivarunkamisetty <70318389+Nagasaivarunkamisetty@users.noreply.github.com>
2020-08-27 05:18:38 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Class_and_objects
+{
+    class Program
+    { 
+        void display()
+        {
+            Console.WriteLine("WELCOME TO C# !");
+        }
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            a.display();
+        }
+    }
+}
